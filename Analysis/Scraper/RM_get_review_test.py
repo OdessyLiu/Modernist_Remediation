68ff3c9a78eff359729254dd6f9141e56dc3dd1a
Qilin Liu <145629416+OdessyLiu@users.noreply.github.com>
2024-09-30 00:34:10 -0700

    review scraper

--- Analysis/Scraper/RM_get_review_test.py
+++ Analysis/Scraper/RM_get_review_test.py
@@ -1,13 +1,25 @@
 import time
 import pandas as pd
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
+import random
+import logging
+
+# Set up logging
+logging.basicConfig(
+    filename='Analysis/Scraper/scraper_log.log',
+    filemode='w',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
 def get_rating(node):
     # Finding the RatingStars element that contants aria-label attribute
@@ -60,14 +72,18 @@ def get_shelves(node):
     return shelves
 
 # Set up and initialize WebDriver
-def init_driver():
+def init_driver(user_agent):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument(user_agent)
+    
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     return driver
 
 # Scrape page and parse HTML
-def fetch_goodreads_reviews(url):
-    driver = init_driver()
+def fetch_goodreads_reviews(url, user_agent):
+    driver = init_driver(user_agent)
     driver.get(url)
     
     time.sleep(3)  # wait for the page to load
@@ -79,6 +95,11 @@ def fetch_goodreads_reviews(url):
 # Click 'Show more reviews' button
 def click_show_more_reviews(driver):
     try:
+        # Check if 'Show more reviews' button is present
+        if len(driver.find_elements(By.XPATH, '//span[@data-testid="loadMore"]/ancestor::button')) == 0:
+            logging.info("No 'Show more reviews' button found, all reviews are loaded.")
+            return False
+        
         # Use WebDriverWait to wait for the 'Show more reviews' button to become clickable
         wait = WebDriverWait(driver, 20)
         show_more_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[@data-testid="loadMore"]/ancestor::button')))
@@ -88,16 +109,16 @@ def click_show_more_reviews(driver):
         time.sleep(3)  # wait for the page to load
         return True
     except TimeoutException:
-        print("Timed out waiting for 'Show more reviews' button to become clickable.")
+        logging.info("Timed out waiting for 'Show more reviews' button to become clickable.")
         return False
     except Exception as e:
-        print(f"Error clicking 'Show more reviews': {e}")
+        logging.info(f"Error clicking 'Show more reviews': {e}")
         return False
 
 
 # Retrieve all reviews from a Goodreads review URL
-def get_all_reviews(url):
-    soup, driver = fetch_goodreads_reviews(url)
+def get_all_reviews(url, user_agent):
+    soup, driver = fetch_goodreads_reviews(url, user_agent)
     reviews = []
 
     while True:
@@ -123,18 +144,42 @@ def get_all_reviews(url):
     driver.quit()
     return reviews
 
-# Save to CSV
-def save_reviews_to_csv(reviews, filename="Analysis/Scraper/RM_output/27132988-thoreau_reviews.csv"):
+# Retrieve goodreads id from metadata file
+def read_goodreads_ids(file_path, sheet_name = "BOOK_SHEET"):
+    df = pd.read_excel(file_path, sheet_name = sheet_name)
+    goodread_ids = df['goodreads_id'].tolist()
+    return goodread_ids
+
+# Generate goodreads review URL from goodreads id
+def generate_goodreads_review_url(goodreads_id):
+    return f"https://www.goodreads.com/book/show/{goodreads_id}/reviews"
+
+# Save each book's reviews to CSV
+def save_reviews_to_csv(reviews, goodreads_id, base_path):
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+    
+    filename = f"{base_path}/{goodreads_id}_reviews.csv"
     df = pd.DataFrame(reviews)
     df.to_csv(filename, index=False)
-    print(f"Reviews saved to {filename}")
+    logging.info(f"Reviews saved to {filename}")
 
-# 测试主函数
+# Main function
 def main():
-    # 使用某本书的 Goodreads 评论页面作为测试
-    url = 'https://www.goodreads.com/book/show/27132988-thoreau/reviews'
-    all_reviews = get_all_reviews(url)
-    save_reviews_to_csv(all_reviews)
+    user_agent  = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+    base_path = "Data/Reviews_Scraped/Raw"
+    metadata_file_path = "Data/Goodreads_Comics_Data/Data_Files/Masterdata_ongoing.xlsx"
+    goodreads_ids = read_goodreads_ids(metadata_file_path)
+    
+    for goodreads_id in goodreads_ids[7:8]:
+        logging.info(f"Scraping reviews for book with Goodreads ID: {goodreads_id}")
+        url = generate_goodreads_review_url(goodreads_id)
+        all_reviews = get_all_reviews(url, user_agent)
+        if len(all_reviews) == 0:
+            logging.info(f"No reviews found for book with Goodreads ID: {goodreads_id}")
+            return
+        save_reviews_to_csv(all_reviews, goodreads_id, base_path)
+        time.sleep(random.uniform(5, 10))
 
 if __name__ == "__main__":
     main()
