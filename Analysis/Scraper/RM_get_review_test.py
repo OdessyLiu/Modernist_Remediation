91e1b07c5119d3eef169e99b1e1d1e9e3c7e5131
Qilin Liu <145629416+OdessyLiu@users.noreply.github.com>
2024-10-06 00:00:39 -0700

    working scraper

--- Analysis/Scraper/RM_get_review_test.py
+++ Analysis/Scraper/RM_get_review_test.py
@@ -15,19 +15,17 @@ import logging
 
 # Set up logging
 logging.basicConfig(
-    filename='Analysis/Scraper/scraper_log.log',
+    filename='Analysis/Scraper/scraper_log2.log',
     filemode='w',
     format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
 def get_rating(node):
-    # Finding the RatingStars element that contants aria-label attribute
     rating_element = node.find('span', {'class': 'RatingStars'})
     if rating_element and 'aria-label' in rating_element.attrs:
-        # Retrieving the ratings from aria-label, i.e., "Rating 3 out of 5"
         rating_text = rating_element['aria-label']
-        rating = rating_text.split()[1]  # Extracting the rating value
+        rating = rating_text.split()[1]
         return rating
     return ''
 
@@ -82,40 +80,33 @@ def init_driver(user_agent):
     return driver
 
 # Scrape page and parse HTML with retries
-def fetch_goodreads_reviews(url, user_agent, retries=3):
+def fetch_goodreads_reviews(url, driver, retries=3):
     for attempt in range(retries):
         try:
-            driver = init_driver(user_agent)
             driver.get(url)
-            
             time.sleep(3)
-            
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
-            
-            return soup, driver
+            return soup
         except Exception as e:
             logging.error(f"Error fetching page: {e}. Retrying... {attempt + 1}/{retries}")
-            time.sleep(random.uniform(10, 20))  # retry after random delay
-        
+            time.sleep(random.uniform(10, 20))
+    
     logging.error(f"Failed to fetch page after {retries} attempts.")
-    return None, None  # return none if failed to fetch page
+    return None
 
 # Click 'Show more reviews' button
 def click_show_more_reviews(driver):
     try:
-        # Check if 'Show more reviews' button is present
         if len(driver.find_elements(By.XPATH, '//span[@data-testid="loadMore"]/ancestor::button')) == 0:
             logging.info("No 'Show more reviews' button found, all reviews are loaded.")
             return False
         
-        # Use WebDriverWait to wait for the 'Show more reviews' button to become clickable
         wait = WebDriverWait(driver, 20)
         show_more_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[@data-testid="loadMore"]/ancestor::button')))
         
-        # Use JavaScript to click the button
         driver.execute_script("arguments[0].click();", show_more_button)
-        time.sleep(3)  # wait for the page to load
+        time.sleep(3)
         return True
     except TimeoutException:
         logging.info("Timed out waiting for 'Show more reviews' button to become clickable.")
@@ -124,59 +115,71 @@ def click_show_more_reviews(driver):
         logging.info(f"Error clicking 'Show more reviews': {e}")
         return False
 
-
 # Retrieve all reviews from a Goodreads review URL
-def get_all_reviews(url, user_agent):
-    soup, driver = fetch_goodreads_reviews(url, user_agent)
+def get_all_reviews(url, goodreads_id, driver, base_path):
+    # initialize csv file path to save
+    filename = f"{base_path}/{goodreads_id}_reviews.csv"
+
+    # Check if file exists to decide whether to write header
+    file_exists = os.path.isfile(filename)
+
+    # initialize review saver
+    soup = fetch_goodreads_reviews(url, driver)
 
     if soup is None:
-        logging.error(f"Failed to retrieve reviews for {url}")
-        return []
+        logging.error(f"Failed to retrieve reviews for {goodreads_id}")
+        return
     
-    reviews = []
+    scraped_review_urls = set()  # Unique identifiers for reviews (user url -- one can only rate a book once)
 
     while True:
+        new_reviews = []
         review_cards = soup.find_all('article', {'class': 'ReviewCard'})
+        
         for review in review_cards:
+            user_url = get_user_url(review)
+
+            if user_url in scraped_review_urls:
+                continue
+
             review_data = {
                 "user_name": get_user_name(review),
-                "user_url": get_user_url(review),
+                "user_url": user_url,
                 "rating": get_rating(review),
                 "date": get_date(review),
                 "text": get_text(review),
                 "likes": get_num_likes(review),
                 "shelves": get_shelves(review)
             }
-            reviews.append(review_data)
+            new_reviews.append(review_data)
+            scraped_review_urls.add(user_url)
+        
+        if len(new_reviews) > 0:
+            df = pd.DataFrame(new_reviews)
+
+            try:
+                # Append to the CSV file, only write header if it's a new file
+                df.to_csv(filename, index=False, mode='a', header=not file_exists)
+                file_exists = True  # Once written, future writes won't need header
+                logging.info(f"Saved {len(new_reviews)} reviews for book ID: {goodreads_id} to {filename}")
+            except Exception as e:
+                logging.error(f"Error writing reviews to file {filename}: {e}")
 
         if not click_show_more_reviews(driver):
-            break  # no more reviews to show, stop scraping
+            break  # No more reviews to show, stop scraping
 
-        # update soup
         soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    driver.quit()
-    return reviews
 
 # Retrieve goodreads id from metadata file
-def read_goodreads_ids(file_path, sheet_name = "BOOK_SHEET"):
-    df = pd.read_excel(file_path, sheet_name = sheet_name)
+def read_goodreads_ids(file_path, sheet_name="BOOK_SHEET"):
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
     goodread_ids = df['goodreads_id'].tolist()
     return goodread_ids
 
 # Generate goodreads review URL from goodreads id
 def generate_goodreads_review_url(goodreads_id):
     return f"https://www.goodreads.com/book/show/{goodreads_id}/reviews"
-
-# Save each book's reviews to CSV
-def save_reviews_to_csv(reviews, goodreads_id, base_path):
-    if not os.path.exists(base_path):
-        os.makedirs(base_path)
-    
-    filename = f"{base_path}/{goodreads_id}_reviews.csv"
-    df = pd.DataFrame(reviews)
-    df.to_csv(filename, index=False)
-    logging.info(f"Reviews saved to {filename}")
 
 def get_random_user_agent(user_agents):
     return random.choice(user_agents)
@@ -187,20 +190,27 @@ def main():
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.3",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.3"]
-    user_agent  = get_random_user_agent(user_agents)
+    
     base_path = "Data/Reviews_Scraped/Raw"
     metadata_file_path = "Data/Goodreads_Comics_Data/Data_Files/Masterdata_ongoing.xlsx"
     goodreads_ids = read_goodreads_ids(metadata_file_path)
-    
-    for goodreads_id in goodreads_ids[18:100]:
+
+    driver = init_driver(get_random_user_agent(user_agents))
+
+    for i, goodreads_id in enumerate(goodreads_ids[130:], start=130):
         logging.info(f"Scraping reviews for book with Goodreads ID: {goodreads_id}")
         url = generate_goodreads_review_url(goodreads_id)
-        all_reviews = get_all_reviews(url, user_agent)
-        if len(all_reviews) == 0:
-            logging.info(f"No reviews found for book with Goodreads ID: {goodreads_id}")
-            continue
-        save_reviews_to_csv(all_reviews, goodreads_id, base_path)
+        get_all_reviews(url, goodreads_id, driver, base_path)
+
+        if (i + 1) % 10 == 0:  # Restart WebDriver after every 10 books
+            logging.info("Restarting WebDriver...")
+            driver.quit()
+            time.sleep(5)
+            driver = init_driver(get_random_user_agent(user_agents))
+
         time.sleep(random.uniform(10, 20))
+
+    driver.quit()
 
 if __name__ == "__main__":
     main()
